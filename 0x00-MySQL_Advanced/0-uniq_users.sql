7b76562c1e062bf5e4bb8e0e1aef4dee6a615f12
7bernie <bernardbassie@gmail.com>
2024-04-24 08:28:55 +0000

    Best band ever!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,4 @@
--- creates a table users with follwing fields
+-- Creates a table users with follwing fields
 -- id, email, name
 CREATE TABLE IF NOT EXISTS users (
 	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
