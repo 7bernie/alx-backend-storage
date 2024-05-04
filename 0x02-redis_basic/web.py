fe203c6c7ae4f1eae995057e033b76e6d5404457
7bernie <bernardbassie@gmail.com>
2024-05-04 09:27:30 +0000

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,79 +1,36 @@
 #!/usr/bin/env python3
 """
-Contains class with methods to create simple pagination from csv data
+Web file
 """
-import csv
-from typing import List
-index_range = __import__('0-simple_helper_function').index_range
+import requests
+import redis
+from functools import wraps
+
+store = redis.Redis()
 
 
-class Server:
-    """Server class to paginate a database of popular baby names.
-    """
-    DATA_FILE = "Popular_Baby_Names.csv"
+def count_url_access(method):
+    """ Decorator counting how many times
+    a Url is accessed """
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
 
-    def __init__(self):
-        self.__dataset = None
+        count_key = "count:" + url
+        html = method(url)
 
-    def dataset(self) -> List[List]:
-        """
-        Reads from csv file and returns the dataset.
-        Returns:
-            List[List]: The dataset.
-        """
-        if self.__dataset is None:
-            with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)
-                dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
 
-        return self.__dataset
 
-    @staticmethod
-    def assert_positive_integer_type(value: int) -> None:
-        """
-        Asserts that the value is a positive integer.
-        Args:
-            value (int): The value to be asserted.
-        """
-        assert type(value) is int and value > 0
-
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Returns a page of the dataset.
-        Args:
-            page (int): The page number.
-            page_size (int): The page size.
-        Returns:
-            List[List]: The page of the dataset.
-        """
-        self.assert_positive_integer_type(page)
-        self.assert_positive_integer_type(page_size)
-        dataset = self.dataset()
-        start, end = index_range(page, page_size)
-        try:
-            data = dataset[start:end]
-        except IndexError:
-            data = []
-        return data
-
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        """
-        Returns a page of the dataset.
-        Args:
-            page (int): The page number.
-            page_size (int): The page size.
-        Returns:
-            List[List]: The page of the dataset.
-        """
-        total_pages = len(self.dataset()) // page_size + 1
-        data = self.get_page(page, page_size)
-        info = {
-            "page": page,
-            "page_size": page_size if page_size <= len(data) else len(data),
-            "total_pages": total_pages,
-            "data": data,
-            "prev_page": page - 1 if page > 1 else None,
-            "next_page": page + 1 if page + 1 <= total_pages else None
-        }
-        return info
+@count_url_access
+def get_page(url: str) -> str:
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
